ad24cb10b6c5590c74f3fc631cbe54767fb76dad
AppVeyor <ci@appveyor.com>
2024-10-30 20:48:27 +0000

    b3361e9

---
+++ service.worker.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+self.addEventListener("install", event => {
+  console.log("Установлен");
+  event.waitUntil(caches.open("BUGGY-SERVICE-CACHE").then(cache => {
+    cache.addAll(["./", "./index.html", "./index.css", "./fallback.html"]);
+  }, error => {
+    console.log(error);
+  }));
+});
+self.addEventListener("activate", () => {
+  console.log("Активирован");
+});
+async function fetchPriorityThenCache(event) {
+  let response;
+  try {
+    response = await fetch(event.request);
+  } catch (err) {
+    response = await caches.match("./fallback.html");
+  }
+  if (response.status === 500) {
+    const cacheResponse = await caches.match(event.request);
+    if (cacheResponse) {
+      return cacheResponse;
+    }
+  }
+  const cache = await caches.open("BUGGY-SERVICE-CACHE");
+  cache.put(event.request, response.clone());
+  return response;
+}
+self.addEventListener("fetch", event => {
+  console.log("Происходит запрос на сервер");
+  event.respondWith(fetchPriorityThenCache(event));
+});
+/******/ })()
+;
